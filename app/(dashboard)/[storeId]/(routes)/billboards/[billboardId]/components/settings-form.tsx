e9606494163e8e02c1d501f5a48b5a0ab551e3a4
Nick Santiago <miltonnsantiago@gmail.com>
2023-11-02 17:34:16 -0500

    data table working

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/settings-form.tsx
@@ -22,8 +22,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "@/components/modals/alert-modal";
-import { ApiAlert } from "@/components/ui/api-alert";
-import { useOrigin } from "@/hooks/use-origin";
 import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
@@ -42,7 +40,6 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
 }) => {
   const params = useParams();
   const router = useRouter();
-  const origin = useOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -75,6 +72,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
         await axios.post(`/api/${params.storeId}/billboards`, data);
       }
       router.refresh();
+      router.push(`/${params.storeId}/billboards`);
       toast.success(toastMessage);
     } catch (error) {
       toast.error("something went wrong.");
